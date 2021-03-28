fe12dfb203d993404cdc70e979ae7c909922a89f
streetturtle <streetturtle@gmail.com>
2021-03-27 21:50:36 -0400

    move docs to spoons folder

--- .github/workflows/get_content.sh
+++ .github/workflows/get_content.sh
@@ -5,8 +5,8 @@ declare -a arr=("people-in-space"
 
 for i in "${arr[@]}"
 do
-  svn --force export https://github.com/fork-my-spoons/"$i".spoon/trunk/README.md ./docs/"$i".md
+  svn --force export https://github.com/fork-my-spoons/"$i".spoon/trunk/README.md ./spoons/"$i".md
   a=$(echo "$i" | tr "-" " ")
 
-  sed -i "1s/^/---\nlayout: default\ntitle: $a\nnav_order: 2\n---\n/" ./docs/"$i".md
+  sed -i "1s/^/---\nlayout: default\ntitle: $a\nnav_order: 2\n---\n/" ./spoons/"$i".md
 done
