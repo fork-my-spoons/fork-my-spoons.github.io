b981ca42f4dbdf6f19e1eb8ca786eb55650690d2
streetturtle <streetturtle@users.noreply.github.com>
2021-03-30 09:26:44 -0400

    Update get_content.sh

--- .github/workflows/get_content.sh
+++ .github/workflows/get_content.sh
@@ -14,7 +14,7 @@ do
   folder_name="${array[1]}"
   file_name="${array[0]}"
 
-  svn --force export "https://github.com/fork-my-spoons/${file_name}.spoon/trunk/README.md ./spoons${folder_name}/${file_name}.md"
+  svn --force export "https://github.com/fork-my-spoons/${file_name}.spoon/trunk/README.md" "./spoons${folder_name}/${file_name}.md"
   a=$(echo "$i" | tr "-" " ")
 
   sed -i "1s/^/---\nlayout: default\ntitle: $a\nnav_order: 2\n---\n/" ./spoons/"$i".md
