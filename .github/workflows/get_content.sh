4adfbf5fb281c0dd90654b05b64798b20637e01e
streetturtle <streetturtle@users.noreply.github.com>
2021-03-30 21:02:22 -0400

    Update get_content.sh

--- .github/workflows/get_content.sh
+++ .github/workflows/get_content.sh
@@ -3,20 +3,27 @@ declare -a arr=("people-in-space"
                 "github-activity"
                 "gitlab-merge-requests"
                 "take-a-break"
-                "spotify-mini-player:/spotify"
-                "spotify-search-and-play:/spotify"
-                "spotify-current-song:/spotify"
+                "spotify-mini-player:/spotify:Spotify"
+                "spotify-search-and-play:/spotify:Spotify"
+                "spotify-current-song:/spotify:Spotify"
                 )
 
 for i in "${arr[@]}"
 do
   IFS=':' read -r -a array <<< "$i"
-  folder_name="${array[1]}"
   file_name="${array[0]}"
+  folder_name="${array[1]}"
+  title="${array[2]}"
 
   mkdir ./spoons/"$folder_name"
   svn --force export "https://github.com/fork-my-spoons/${file_name}.spoon/trunk/README.md" "./spoons${folder_name}/${file_name}.md"
   a=$(echo "$file_name" | tr "-" " ")
 
-  sed -i "1s/^/---\nlayout: default\ntitle: $a\nnav_order: 2\n---\n/" ./spoons"${folder_name}/${file_name}".md
+  if [ -z "$folder_name" ]
+  then
+    sed -i "1s/^/---\nlayout: default\ntitle: $a\nnav_order: 2\n---\n/" ./spoons"${folder_name}/${file_name}".md
+  else
+    sed -i "1s/^/---\nlayout: default\ntitle: $a\nnav_order: 2\nparent: $title\n---\n/" ./spoons"${folder_name}/${file_name}".md
+  fi
+
 done
